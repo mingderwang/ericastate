cee4dbaa34ca0bc826b05c88394bd5a0ab034fec
Ming-der Wang <mingderwang@gmail.com>
2023-12-30 17:16:11 +0800

    test flowbite

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx(), tailwind()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import flowbite from 'astro-flowbite'
+
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [flowbite()],
+}
